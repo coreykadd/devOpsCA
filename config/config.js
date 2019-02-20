efdd42799a20a6b7a04969b3b6dd84a9860c4124
cmangan <coreymangan22@live.ie>
2019-02-20 14:36:43 +0000

    config

--- config/config.js
+++ config/config.js
@@ -7,5 +7,5 @@ CONFIG.db_port = process.env.DB_PORT || '27017';
 
 CONFIG.RDS_HOSTNAME = process.env.RDS_HOSTNAME || 'aa1vc3xj4c3k7kv.cwwnavucu4nf.eu-west-1.rds.amazonaws.com';
 CONFIG.RDS_USERNAME = process.env.RDS_USERNAME || 'corey';
-CONFIG.RDS_PASSWORD = process.env.RDS_PASSWORD || 'xa4e6epm';
+CONFIG.RDS_PASSWORD = process.env.RDS_PASSWORD || 'Testing22';
 CONFIG.RDS_PORT = process.env.RDS_PORT || '3306';
